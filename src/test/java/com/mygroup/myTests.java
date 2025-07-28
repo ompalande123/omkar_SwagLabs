b508de2b754478ae18d46e81416c2be33ea10e9d
ompalande123 <ompalande123@gmail.com>
2025-07-28 23:20:31 +0530

    second commit

--- src/test/java/com/mygroup/myTests.java
+++ src/test/java/com/mygroup/myTests.java
@@ -2,6 +2,7 @@ package com.mygroup;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 class MyTests {
@@ -14,6 +15,11 @@ class MyTests {
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/v1/");
 
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+    
     }
 
 }
